25115199dffa566aea351a585395ba185d083778
Sharyafetdinova Sasha <82018764+a-sharyafetdinova@users.noreply.github.com>
2022-07-29 02:24:32 +0300

    Update click.js

--- click.js
+++ click.js
@@ -1,4 +1,8 @@
 function letter_with_mutation(letter) {
 	alert('Вы нажали на '+letter.innerHTML);
-
+	var letter_id = Number(letter.id)
+	console.log(letter_id)
+	var table_element = document.getElementsByClassName('table')[letter_id]
+	console.log(table_element)
+	table_element.style.background == "yellow"
 }
